d1f064e3bc24e5c4c00067e704529730b1d6f093
Chris Burke <burke1791@gmail.com>
2020-01-11 06:54:14 -0600

    imported and configured AWS amplify

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Amplify from 'aws-amplify';
+import amplifyConfig from './utilities/amplifyConfig';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: amplifyConfig.cognito.REGION,
+    userPoolId: amplifyConfig.cognito.USER_POOL_ID,
+    identityPoolId: amplifyConfig.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: amplifyConfig.cognito.APP_CLIENT_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'testLambda',
+        endpoint: amplifyConfig.apiGateway.URL,
+        region: amplifyConfig.apiGateway.REGION
+      }
+    ]
+  }
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
