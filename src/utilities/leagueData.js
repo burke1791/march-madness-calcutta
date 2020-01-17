f1662ee61aea6f36369e9907ccce4ba4aaa68e3d
Chris Burke <burke1791@gmail.com>
2020-01-17 03:00:33 -0600

    added functions for downloading and packaging league summary info

--- src/utilities/leagueData.js
+++ src/utilities/leagueData.js
@@ -3,22 +3,20 @@ import { ENDPOINTS, NOTIF } from './constants';
 import Axios from 'axios';
 import Pubsub from './pubsub';
 
-export function createLeague(name, password, year) {
-  let session = User.session.idToken.jwtToken || '';
+let Data = {};
 
+export function createLeague(name, password, year) {
   let league = {
     name: name,
     password: password,
     year: year
   };
-  console.log(session);
-  console.log(league);
 
   Axios({
     method: 'POST',
     url: process.env.REACT_APP_API_URL + ENDPOINTS.NEW_LEAGUE,
     headers: {
-      'x-cognito-token': session
+      'x-cognito-token': User.session.idToken.jwtToken || ''
     },
     data: league
   }).then(response => {
@@ -27,4 +25,46 @@ export function createLeague(name, password, year) {
   }).catch(error => {
     console.log(error);
   });
+}
+
+export function getLeagueSummaries() {
+  Axios({
+    method: 'GET',
+    url: process.env.REACT_APP_API_URL + ENDPOINTS.LEAGUE_SUMMARIES,
+    headers: {
+      'x-cognito-token': User.session.idToken.jwtToken || ''
+    }
+  }).then(response => {
+    console.log(response);
+    Data.leagues = packageLeagueSummaries(response.data);
+    Pubsub.publish(NOTIF.LEAGUE_SUMMARIES_FETCHED, null);
+  }).catch(error => {
+    console.log(error);
+  });
+}
+
+
+function packageLeagueSummaries(data) {
+  let leagues = data.map(league => {
+    let leagueObj = {
+      id: league.leagueId,
+      name: league.name,
+      buyIn: league.naturalBuyIn + league.taxBuyIn,
+      payout: league.return,
+      role: league.role,
+      auctionId: league.auctionId
+    };
+
+    return leagueObj;
+  });
+
+  return leagues;
+}
+
+export function clearDataOnSignout() {
+  Data = {};
+}
+
+export {
+  Data
 }
