4497de11d46e6fb46717f1dede35f06711d49eaa
Chris Burke <burke1791@gmail.com>
2020-03-21 08:44:46 -0500

    refactored layout

--- src/components/leagueHome/leagueHomeCards.js
+++ src/components/leagueHome/leagueHomeCards.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Avatar, Row } from 'antd';
+import { Card, Avatar, Row, Col } from 'antd';
 import { UserOutlined, DollarOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import { formatMoney } from '../../utilities/helper';
@@ -9,31 +9,39 @@ const { Meta } = Card;
 function LeagueHomeCards(props) {
   
   return (
-    <Row type='flex' justify='center'>
-      <Card style={{ width: '200px', textAlign: 'center' }}>
-        <Meta
-          avatar={<Avatar icon={<UserOutlined />} />}
-          title={`${props.userCount} Users`}
-        />
-      </Card>
-      <Card style={{ width: '200px', textAlign: 'center' }}>
-        <Meta
-          avatar={<Avatar icon={<DollarOutlined />} />}
-          title={formatMoney(props.prizepool)}
-        />
-      </Card>
-      <Card style={{ width: '200px', textAlign: 'center' }}>
-        <Meta
-          avatar={<Avatar icon={<UserOutlined />} />}
-          title={`${props.userCount} Users`}
-        />
-      </Card>
-      <Card style={{ width: '200px', textAlign: 'center' }}>
-        <Meta
-          avatar={<Avatar icon={<DollarOutlined />} />}
-          title={formatMoney(props.prizepool)}
-        />
-      </Card>
+    <Row type='flex' justify='center' gutter={[12, 8]}>
+      <Col md={5} xxl={3}>
+        <Card style={{ textAlign: 'center' }}>
+          <Meta
+            avatar={<Avatar icon={<UserOutlined />} />}
+            title={`${props.userCount} Users`}
+          />
+        </Card>
+      </Col>
+      <Col md={5} xxl={3}>
+        <Card style={{ textAlign: 'center' }}>
+          <Meta
+            avatar={<Avatar icon={<DollarOutlined />} />}
+            title={formatMoney(props.prizepool)}
+          />
+        </Card>
+      </Col>
+      <Col md={5} xxl={3}>
+        <Card style={{ textAlign: 'center' }}>
+          <Meta
+            avatar={<Avatar icon={<UserOutlined />} />}
+            title={`${props.userCount} Users`}
+          />
+        </Card>
+      </Col>
+      <Col md={5} xxl={3}>
+        <Card style={{ textAlign: 'center' }}>
+          <Meta
+            avatar={<Avatar icon={<DollarOutlined />} />}
+            title={formatMoney(props.prizepool)}
+          />
+        </Card>
+      </Col>
     </Row>
   )
 }
