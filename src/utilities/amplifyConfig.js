df641ecb5271bad90ef34751a9e7e4ac8fbb2cf3
Chris Burke <burke1791@gmail.com>
2020-07-18 12:26:26 -0500

    fixing a really really stupid typo

--- src/utilities/amplifyConfig.js
+++ src/utilities/amplifyConfig.js
@@ -3,6 +3,6 @@ export default {
     REGION: process.env.REACT_APP_BUILD_ENV == 'dev' ? process.env.REACT_APP_DEV_AUTH_REGION : process.env.REACT_APP_AUTH_REGION,
     USER_POOL_ID: process.env.REACT_APP_BUILD_ENV == 'dev' ? process.env.REACT_APP_DEV_AUTH_USER_POOL_ID : process.env.REACT_APP_AUTH_USER_POOL_ID,
     APP_CLIENT_ID: process.env.REACT_APP_BUILD_ENV == 'dev' ? process.env.REACT_APP_DEV_AUTH_APP_CLIENT_ID : process.env.REACT_APP_AUTH_APP_CLIENT_ID,
-    AUTH_FLOW: process.env.REACT_APP_BUILD_ENV == 'USER_PASSWORD_AUTH'
+    AUTH_FLOW: 'USER_PASSWORD_AUTH'
   }
 }
