24db8bfbb0c7b2a3d7f210bb2dc369f9dee08132
Chris Burke <burke1791@gmail.com>
2020-11-14 08:29:02 -0600

    removed comments

--- src/context/leagueSettingsContext.js
+++ src/context/leagueSettingsContext.js
@@ -3,19 +3,6 @@ import ContextSkeleton from './skeleton';
 
 const settingsContext = new ContextSkeleton('settingsContext', false);
 
-
-// function SettingsProvider({children}) {
-//   return settingsContext.ContextProvider({children});
-// }
-
-// function useSettingsState() {
-//   return settingsContext.useContextState;
-// }
-
-// function useSettingsDispatch() {
-//   return settingsContext.useContextDispatch;
-// }
-
 const StateContext = createContext();
 const DispatchContext = createContext();
 
