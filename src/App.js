34cba1e8782824e802b21053b2d420b5b30f7f97
Chris Burke <burke1791@gmail.com>
2021-02-05 15:31:42 -0600

    removed admin page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Main from './components/main/main';
 import { Layout } from 'antd';
 import League from './components/league/league';
 import LandingPage from './landingPage/landingPage';
-import Admin from './components/adminPage/admin';
 import { LeagueProvider } from './context/leagueContext';
 import { AuthProvider } from './context/authContext';
 import { SettingsProvider } from './context/leagueSettingsContext';
@@ -31,7 +30,6 @@ function App() {
               <TournamentProvider>
                 <Router>
                   <LandingPage path='/' />
-                  <Admin path='/admin' />
                   <Main path='/home' />
                   <League path='/leagues/:leagueId/*' />
                 </Router>
