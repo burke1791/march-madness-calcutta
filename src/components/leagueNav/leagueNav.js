4195fd11d2e7beb5f513459d98f5b3dcc3bfce0d
Chris Burke <burke1791@gmail.com>
2022-03-19 03:18:38 -0500

    MMC-33: fixed menu item bug and added an auth check for downloading bracket data

--- src/components/leagueNav/leagueNav.js
+++ src/components/leagueNav/leagueNav.js
@@ -3,18 +3,37 @@ import Pubsub from '../../utilities/pubsub';
 import { NOTIF } from '../../utilities/constants';
 import { Menu, Layout } from 'antd';
 import 'antd/dist/antd.css';
-import { navigate } from '@reach/router';
+import { navigate, useLocation } from '@reach/router';
 import { useLeagueState } from '../../context/leagueContext';
+import { parseLeaguePathName } from '../../utilities/helper';
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
+
+const TOP_LEVEL_MENU_ITEMS = {
+  HOME: 'leagueHome',
+  AUCTION: 'auction',
+  BRACKET: 'bracket',
+  MESSAGE_BOARD: 'messageBoard',
+  SETTINGS: 'settingSub'
+};
+
+const SETTINGS_SUBMENU_ITEMS = {
+  LEAGUE: 'settings/league',
+  AUCTION: 'settings/auction',
+  SEED_GROUPS: 'settings/seed_groups',
+  PAYOUT: 'settings/payout'
+}
 
 function LeagueNav() {
 
   const [displaySider, setDisplaySider] = useState(true);
   const [collapsed, setCollapsed] = useState(false);
+  const [selectedKeys, setSelectedKeys] = useState([]);
 
   const { leagueId, hasBracketPage } = useLeagueState();
+
+  const location = useLocation();
 
   useEffect(() => {
     Pubsub.subscribe(NOTIF.LEAGUE_MENU_TOGGLE, LeagueNav, handleMenuToggle);
@@ -23,6 +42,10 @@ function LeagueNav() {
       Pubsub.unsubscribe(NOTIF.LEAGUE_MENU_TOGGLE, LeagueNav);
     });
   }, []);
+
+  useEffect(() => {
+    setSelectedKeys(getSelectedMenuItem());
+  }, [location.pathname])
 
   const handleCollapse = (flag, type) => {
     if (window.innerWidth >= 992) {
@@ -37,19 +60,35 @@ function LeagueNav() {
     setDisplaySider(!displaySider);
   }
 
+  const getSelectedMenuItem = () => {
+    console.log(location);
+    const pathData = parseLeaguePathName(location.pathname);
+
+    if (pathData.menuItem == undefined) {
+      return [TOP_LEVEL_MENU_ITEMS.HOME];
+    }
+
+    if (pathData.subMenuItem == undefined) {
+      return [pathData.menuItem];
+    }
+
+    return [`${pathData.menuItem}/${pathData.subMenuItem}`];
+  }
+
   const handleLeagueNavClick = (event) => {
     handleCollapse(true, 'menuClick');
+    // setSelectedKeys([event.key]);
 
     if (leagueId) {
-      if (event.key == 'leagueHome') {
+      if (event.key == TOP_LEVEL_MENU_ITEMS.HOME) {
         navigate(`/leagues/${leagueId}`);
-      } else if (event.key == 'auction') {
+      } else if (event.key == TOP_LEVEL_MENU_ITEMS.AUCTION) {
         navigate(`/leagues/${leagueId}/auction`);
       } else if (event.key == 'bracket') {
         navigate(`/leagues/${leagueId}/bracket`);
-      } else if (event.key == 'messageBoard') {
+      } else if (event.key == TOP_LEVEL_MENU_ITEMS.MESSAGE_BOARD) {
         // navigate(`/leagues/${leagueId}/message_board`)
-      } else if (event.keyPath[1] == 'settingSub') {
+      } else if (event.keyPath[1] == TOP_LEVEL_MENU_ITEMS.SETTINGS) {
         navigate(`/leagues/${leagueId}/${event.key}`);
       }
     } else {
@@ -85,6 +124,7 @@ function LeagueNav() {
           defaultSelectedKeys={['leagueHome']}
           style={{ height: '100%', borderRight: 0 }}
           defaultOpenKeys={['settingSub']}
+          selectedKeys={selectedKeys}
         >
           <Menu.Item key='leagueHome'>
             League Home
--- src/components/tournament/tournament.js
+++ src/components/tournament/tournament.js
@@ -12,6 +12,7 @@ import { tournamentServiceHelper } from '../../services/tournament/helper';
 import BracketFactory from '../bracket/bracket';
 import { leagueServiceHelper } from '../../services/league/helper';
 import { useTournamentDispatch } from '../../context/tournamentContext';
+import { useAuthState } from '../../context/authContext';
 
 const { Content } = Layout;
 const { Option } = Select;
@@ -24,14 +25,15 @@ function Tournament() {
   const [selectedUser, setSelectedUser] = useState(null);
 
   const { leagueId, leagueName, tournamentName, tournamentRegimeName } = useLeagueState();
+  const { authenticated } = useAuthState();
   const tournamentDispatch = useTournamentDispatch();
 
   useEffect(() => {
-    if (leagueId) {
+    if (leagueId && authenticated) {
       fetchTournamentTree();
       fetchLeagueUsers();
     }
-  }, [leagueId]);
+  }, [leagueId, authenticated]);
 
   const secondaryHeaderText = () => {
     let text = tournamentName;
