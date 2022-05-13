77bdc90d6e98dfba67f226b5c36aecdad4dbbca2
Chris Burke <burke1791@gmail.com>
2022-05-13 03:04:54 -0500

    MMC-79: hiding logo block if there is no logo url'

--- src/components/team/team.js
+++ src/components/team/team.js
@@ -11,11 +11,26 @@ const defaultImgStyle = {
   width: 'auto'
 };
 
+/**
+ * @typedef TeamProps
+ * @property {Object} style - React style prop; for the top-level div
+ * @property {Object} imgStyle - React style prop; for the logo div
+ * @property {String} imgSrc - url for the team logo
+ * @property {String} name - team name
+ */
+
+/**
+ * @component Team
+ * @param {TeamProps} props 
+ */
 function Team(props) {
 
   return (
     <div style={{...defaultStyle, ...props.style}}>
-      <div style={{ width: props.imgStyle?.maxWidth ? props.imgStyle.maxWidth + 10 : '100%', textAlign: 'center', display: 'inline-block' }}>
+      <div
+        style={{ width: props.imgStyle?.maxWidth ? props.imgStyle.maxWidth + 10 : '100%', textAlign: 'center', display: 'inline-block' }}
+        hidden={!props.imgSrc}
+      >
         <img src={props.imageSrc} style={{...defaultImgStyle, ...props.imgStyle}} />
       </div>
       <div style={{ marginLeft: 8, display: 'inline-block' }}>
