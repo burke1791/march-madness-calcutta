e8e860862e4394e0bffe82f20ded785e5165dd87
Chris Burke <burke1791@gmail.com>
2022-06-23 08:46:25 -0500

    MMC-56: forgot to save

--- src/components/auctionRulesGroup/auctionGroup.js
+++ src/components/auctionRulesGroup/auctionGroup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLeagueState } from '../../context/leagueContext';
+import { leagueServiceHelper } from '../../services/league/helper';
 import { LEAGUE_SERVICE_ENDPOINTS } from '../../utilities/constants';
-import AuctionRules from '../auctionRules/auctionRules';
 
 function AuctionGroup() {
 
@@ -10,7 +10,9 @@ function AuctionGroup() {
   return (
     <AuctionRules
       getEndpoint={`${LEAGUE_SERVICE_ENDPOINTS.GET_LEAGUE_SEED_GROUPS}/${leagueId}`}
+      getPostProcess={leagueServiceHelper.packageLeagueSeedGroups}
       postEndpoint={`${LEAGUE_SERVICE_ENDPOINTS.NEW_LEAGUE_SEED_GROUP}`}
+      ruleKey='groupId'
     >
 
     </AuctionRules>
